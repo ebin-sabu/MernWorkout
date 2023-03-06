89eaa2f1861969d98e6fa00e24665f1abf5d2717
ebin-sabu <ebin.pereppadan01@gmail.com>
2023-03-06 19:38:51 +0000

    Second&ThirdTutorial

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const WorkoutRoutes = require('./routes/workout')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -19,9 +20,15 @@ app.get('/', (req, res) =>{
 */
 app.use('/api/workouts', WorkoutRoutes)
 
-
-app.listen(process.env.PORT,() =>{
-    console.log('listening on port',process.env.PORT)
-})
+// Connecting to mongodb Database
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        app.listen(process.env.PORT,() =>{
+            console.log('Connected to db & listening on port',process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 process.env
