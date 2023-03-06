49b13180200bca9c94604afafff3fef6b6b78d4c
ebin-sabu <ebin.pereppadan01@gmail.com>
2023-03-06 21:12:27 +0000

    Adding data to database

--- routes/workout.js
+++ routes/workout.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/WorkoutModel')
 
 const router = express.Router()
 
@@ -10,8 +11,14 @@ router.get('/:id',(req, res) =>{
     res.json({mssg: 'GET a single Workout'})
 })
 
-router.post('/',(req, res) =>{
-    res.json({mssg: 'POST a new Workout'})
+router.post('/',async (req, res) =>{
+    const{title, weight, reps} = req.body
+    try{
+        const workout = await Workout.create({title, weight, reps})
+        res.status(200).json(workout)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
 })
 
 router.delete('/:id',(req, res) =>{
