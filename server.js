692ccc22bdf72b2a66199f6131bd1e367f02217a
ebin-sabu <ebin.pereppadan01@gmail.com>
2023-03-06 19:02:29 +0000

    Second Tutorial

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const WorkoutRoutes = require('./routes/workout')
 
 const app = express()
 
@@ -8,10 +9,13 @@ app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
 })
-
+/*
 app.get('/', (req, res) =>{
     res.json({mssg: 'App Starting up!'})
 })
+*/
+app.use('/api/workouts', WorkoutRoutes)
+
 
 app.listen(process.env.PORT,() =>{
     console.log('listening on port',process.env.PORT)
